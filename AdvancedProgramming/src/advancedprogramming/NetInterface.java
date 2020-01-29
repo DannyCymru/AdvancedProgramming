3c2c7bdd904845422bb90cebd297f93e9319763c
Danny <danny@erebus.cymru>
2020-01-29 23:44:33 +0000

    Made an error while fixing it to standards. Reverted and fixed it correctly

--- AdvancedProgramming/src/advancedprogramming/NetInterface.java
+++ AdvancedProgramming/src/advancedprogramming/NetInterface.java
@@ -22,40 +22,40 @@ public static void main(String args[]) throws SocketException
     {
         
         ArrayList<NetworkInterface> NiList = Collections.list(NetworkInterface.getNetworkInterfaces()); 
-        ArrayList<String> Data = new ArrayList<>();
+        ArrayList<String> data = new ArrayList<>();
         
         for (NetworkInterface Ips1 : NiList)  
         { 
              
-             Enumeration<InetAddress> Iplist = Ips1.getInetAddresses();
+             Enumeration<InetAddress> ipList = Ips1.getInetAddresses();
              
-                while (Iplist.hasMoreElements())  
+                while (ipList.hasMoreElements())  
                 {   
-                        String Intel = Iplist.nextElement().toString();
-                        Data.add(Intel);
+                        String Intel = ipList.nextElement().toString();
+                        data.add(Intel);
                 } 
         }
        
-        int SizeList = Data.size();
+        int SizeList = data.size();
         int i = 0;
         int j = 0;
-        String LoopBackAdre=Data.get(SizeList - 1);
+        String LoopBackAdre=data.get(SizeList - 1);
         
-        ArrayList<String> IPs = new ArrayList<String>();
+        ArrayList<String> ipArray = new ArrayList<String>();
         
         while(i != SizeList)
         {
-            String ItemCheked = Data.get(i);
+            String itemChecked = data.get(i);
             
-            if(ItemCheked.contains(".") && ItemCheked != LoopBackAdre) 
+            if(itemChecked.contains(".") && itemChecked != LoopBackAdre) 
             {
-                IPs.add(Data.get(i));
+                ipArray.add(data.get(i));
                 
             }
             
             i++;
         }
-        System.out.println("IPs: "+ IPs);
+        System.out.println("ipArray: "+ ipArray);
         
     }
     
