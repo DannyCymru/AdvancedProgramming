2f21d6acec2fb65316a086faf1b231f32be2929d
barney <32000608+morzen@users.noreply.github.com>
2020-01-30 16:06:07 +0000

    comments first draft
    should I add more comments or maybe do it differently? I fear it might not be enough comments but if I detail everything it might be too much

--- AdvancedProgramming/src/advancedprogramming/NetInterface.java
+++ AdvancedProgramming/src/advancedprogramming/NetInterface.java
@@ -21,29 +21,31 @@ public class NetInterface{
 public static void main(String args[]) throws SocketException  
     {
         
-        ArrayList<NetworkInterface> NiList = Collections.list(NetworkInterface.getNetworkInterfaces()); 
-        ArrayList<String> data = new ArrayList<>();
+        ArrayList<NetworkInterface> NiList = Collections.list(NetworkInterface.getNetworkInterfaces());//grab the network interface and store as a list
+        ArrayList<String> data = new ArrayList<>();                                                    // network interface is a class it s made up of names and list of IP addresses 
         
         for (NetworkInterface Ips1 : NiList)  
         { 
              
-             Enumeration<InetAddress> ipList = Ips1.getInetAddresses();
+             Enumeration<InetAddress> ipList = Ips1.getInetAddresses(); //InetAddress is a method used to get IP address of any hostname which in this case would be the IPs of where the code run
              
                 while (ipList.hasMoreElements())  
                 {   
-                        String Intel = ipList.nextElement().toString();
-                        data.add(Intel);
+                        String Intel = ipList.nextElement().toString(); // here i am changing all the data from ipList which are of type InetAddress and changing 
+                                                                        //them to string as it is easier for me to manipulate
+                        data.add(Intel);                                // and then add them to an arraylist
                 } 
         }
        
         int SizeList = data.size();
         int i = 0;
-        int j = 0;
-        String LoopBackAdre=data.get(SizeList - 1);
         
+        String LoopBackAdre=data.get(SizeList - 1);// the minus one here is used to avoid an outof bound error though i wish to make
+                                                   //LoopBackAdre = /127.0.0.1 to remove the loopbackaddress from the list but it appear to not recogonize it when i do so  (will improve when i can )        
         ArrayList<String> ipArray = new ArrayList<String>();
         
-        while(i != SizeList)
+        while(i != SizeList)                        // this while loop should be removing all the wrong addresses and the loopback address
+                                                    // by removing all address with "." in and by removing the last item of the list data (which is technically the loopback though i need to check)
         {
             String itemChecked = data.get(i);
             
