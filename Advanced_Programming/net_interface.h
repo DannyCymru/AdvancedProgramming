3104d1546013988e2839a31421e6e8c8d194fe1d
Danny <danny@erebus.cymru>
2020-03-04 15:25:17 +0000

    Simple UDP socket program that sneds user input as datagrams to local broadcast. Added comments to explain some of the more complicated aspects of the code and added a net interface debugging function.

--- Advanced_Programming/net_interface.h
+++ Advanced_Programming/net_interface.h
@@ -1,11 +1,15 @@
 #ifndef NET_INTERFACE_H
 #define NET_INTERFACE_H
-
+#include <QNetworkInterface>
+#include <QNetworkConfiguration>
+#include <QtNetwork>
+#include <QDebug>
 
 class net_interface
 {
 public:
     net_interface();
+    void net_required();
 
 private:
 };
