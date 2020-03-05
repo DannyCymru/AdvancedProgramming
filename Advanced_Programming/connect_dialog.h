b20e1353ddf9ab4df45932e7eee05a364b382754
Danny <danny@erebus.cymru>
2020-03-05 05:42:59 +0000

    Created files for a second form to connect to a server

---
+++ Advanced_Programming/connect_dialog.h
@@ -0,0 +1,25 @@
+#ifndef CONNECT_DIALOG_H
+#define CONNECT_DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class connect_dialog;
+}
+
+class connect_dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit connect_dialog(QWidget *parent = nullptr);
+    ~connect_dialog();
+
+private slots:
+    void on_buttonBox_accepted();
+
+private:
+    Ui::connect_dialog *ui;
+};
+
+#endif // CONNECT_DIALOG_H
